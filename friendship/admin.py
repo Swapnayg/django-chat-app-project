46a7ac1bef41bd5f0605d32f11b338a2036d2f37
Swapnayg <86843363+Swapnayg@users.noreply.github.com>
2022-07-13 21:50:57 +0530

    Update admin.py

--- friendship/admin.py
+++ friendship/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Friendship
 
 # Register your models here.
+class AdminFriendship(admin.ModelAdmin):
+    list_display = ['user','friend','status','timestamp']
+
+admin.site.register(Friendship, AdminFriendship)
