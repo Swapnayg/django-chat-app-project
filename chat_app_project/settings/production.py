b0bce8098e7e081727ad3273173c29073b10315e
Swapnayg <86843363+Swapnayg@users.noreply.github.com>
2022-07-13 21:26:46 +0530

    Update production.py

--- chat_app_project/settings/production.py
+++ chat_app_project/settings/production.py
@@ -8,7 +8,16 @@ DEBUG = env("DEBUG")
 
 SECRET_KEY = env("SECRET_KEY")
 
-DATABASES = {"default": env.db("DATABASE_URL")}
+DATABASES = {
+    "default": {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ddfj19h06063va', 
+        'USER': 'lgntkaraxrrtti', 
+        'PASSWORD': '8fb5d9a60d59573d40d109ec6a5af9bbb99f3ded8eacf9b689f0222dc900b05f',
+        'HOST': 'ec2-3-223-169-166.compute-1.amazonaws.com', 
+        'PORT': '5432',
+    }
+}
 
 ALLOWED_HOSTS = ['www.chat-djangoapp.herokuapp.com','chat-djangoapp.herokuapp.com']
 
