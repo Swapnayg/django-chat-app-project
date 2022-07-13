bdf27ec90989c0bd2c29b606a6c436b6a3fe8f0d
Swapnayg <86843363+Swapnayg@users.noreply.github.com>
2022-07-13 20:08:59 +0530

    Update urls.py

--- chat_app_project/urls.py
+++ chat_app_project/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.conf import settings
+from django.contrib import admin
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -22,6 +23,7 @@ urlpatterns = [
     path("", include("users.urls")),
     path("", include("friendship.urls")),
     path("", include("chat.urls")),
+    path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = "main.views.error404"
