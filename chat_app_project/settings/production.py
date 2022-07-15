46750d5b9f9ef159ceaa08e94bc4923d761981d8
Swapnayg <86843363+Swapnayg@users.noreply.github.com>
2022-07-15 11:53:53 +0530

    Update production.py

--- chat_app_project/settings/production.py
+++ chat_app_project/settings/production.py
@@ -15,8 +15,8 @@ ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [env("REDIS_URL")],
-        },
+        #"CONFIG": {
+            #"hosts": [env("REDIS_URL")],
+        #},
     }
 }
